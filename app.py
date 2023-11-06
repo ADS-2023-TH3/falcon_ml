5d14c6722b7cef8fd20d36c9d7b94e5ea03a0d65
alex <alex-pv01@proton.me>
2023-11-07 00:04:45 +0100

    adding training demo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from your_recommender_module import get_recommendations  # Import your recommender function
+import spotlight as sl 
+#from your_recommender_module import get_recommendations  # Import your recommender function
 
 # Streamlit app code
 st.title('Movie Recommender App')
@@ -9,9 +10,9 @@ user_movie_title = st.text_input('Enter a movie title:')
 
 if user_movie_title:
     # Get recommendations based on user input
-    recommendations = get_recommendations(user_movie_title)
+    #recommendations = get_recommendations(user_movie_title)
     
     # Display recommendations to the user
     st.subheader('Top 5 Recommendations:')
-    for idx, movie in enumerate(recommendations[:5]):
-        st.write(f'{idx + 1}. {movie}')
+    #for idx, movie in enumerate(recommendations[:5]):
+    #    st.write(f'{idx + 1}. {movie}')
