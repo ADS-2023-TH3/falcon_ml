e71f8900f03553504c03e9ec8fd5890605831330
flaviaferrus <flaviaferrus@gmail.com>
2023-11-10 20:53:23 +0100

    Update  ImplicitSec_rec_model.py

--- src/ImplicitSec_rec_model.py
+++ src/ImplicitSec_rec_model.py
@@ -11,7 +11,7 @@ import pandas as pd
 def load_data_to_sequences(variant = '20M', 
                             max_sequence_length = 200,
                             min_sequence_length = 20,
-                            step_size = 200):
+                            step_size = 200, df_split = False):
     
     """This functions loads the movielens dataset from the spotlight package
     
@@ -22,6 +22,7 @@ def load_data_to_sequences(variant = '20M',
         max_sequence_length (int) = 200 by default.
         min_sequence_length (int) = 20 by default.
         step_size (int) = 200 by default.
+        df_split (boolean): False by default. Determines whether the train/test split is conducted
     Returns:
         train (sequence)
         test (sequence) 
@@ -29,16 +30,23 @@ def load_data_to_sequences(variant = '20M',
     """
     random_state = np.random.RandomState(100)
     dataset = get_movielens_dataset(variant=variant)
-    train, test = user_based_train_test_split(dataset,
-                                              random_state=random_state)
-    train = train.to_sequence(max_sequence_length=max_sequence_length,
-                              min_sequence_length=min_sequence_length,
-                              step_size=step_size)
-    test = test.to_sequence(max_sequence_length=max_sequence_length,
-                            min_sequence_length=min_sequence_length,
-                            step_size=step_size)
+    if df_split:
+        train, test = user_based_train_test_split(dataset,
+                                                random_state=random_state)
+        train = train.to_sequence(max_sequence_length=max_sequence_length,
+                                min_sequence_length=min_sequence_length,
+                                step_size=step_size)
+        test = test.to_sequence(max_sequence_length=max_sequence_length,
+                                min_sequence_length=min_sequence_length,
+                                step_size=step_size)
+        
+        return train, test
     
-    return train, test
+    else:
+        dataset = dataset.to_sequence(max_sequence_length=max_sequence_length,
+                                min_sequence_length=min_sequence_length,
+                                step_size=step_size) 
+        return dataset
 
 def train_ImplicitSec_model(train, model_type = 'cnn', save_model = True):
     """Function that trains and saves the recommender model ImplicitSequenceModel() 
