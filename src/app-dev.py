f6b2664bf01f220d4a3ede5e9cac001dbe862176
alex <alex-pv01@proton.me>
2023-11-14 12:55:08 +0100

    modifying docker-deploy

--- src/app-dev.py
+++ src/app-dev.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('/falcon_ml/src')
-
 import streamlit as st
 import pandas as pd
 from popular_rec_model import *
@@ -10,25 +7,42 @@ import torch
 def main():
     st.title('Movie Recommender')
 
-    data = pd.read_csv('/falcon_ml/Data/title_films.csv')
+    # First we read the data and get the list of movies, we will need it to desplay in the select box
+    # We also define the list of Genres
+    data = pd.read_csv('../Data/title_films.csv')
     movies = data['title'].values
     genres = ['', 'Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
               'Film-Noir', 'Horror', 'IMAX', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War',
               'Western']
 
-    with open('/falcon_ml/trained_models/popular_rec_model.pkl', 'rb') as file:
+    # load the models
+    # popular recommender
+    with open('../trained_models/popular_rec_model.pkl', 'rb') as file:
         popul_model = pd.read_pickle(file)
 
-    imp_sec_model = torch.load('/falcon_ml/trained_models/ImplicitSec_rec_model.pth')
+    # General model
+    imp_sec_model = torch.load('../trained_models/ImplicitSec_rec_model.pth')
 
+    # What comes next is a form. A form is a set of input elements and a submit button.
+    # When you click submit the form will return selected movies and genre and will run the code
+    # to give a recommendation according to the input data.
     with st.form("my_form"):
         selected_movies = st.multiselect("Choose Movies", movies)
         selected_genre = st.selectbox("Genre", genres)
 
+        # We have to change the blank space for None for the models to work correctly
         if selected_genre == '':
             selected_genre = None
+        # submit button.
         submitted = st.form_submit_button("Submit")
 
+        # The following logic structure selects the model to use.
+        # 1. The user selcts movies --> General model
+        # 2. No movies:
+        # 2.1 Genre selected --> popular recomender
+        # 2.2 No Genre --> Our recommendations
+        # There are a series of transformations from movie to movie id and viceversa for the
+        # inputs/outputs to work correctly
         if submitted:
             if (len(selected_movies) == 0):
                 if selected_genre == None:
