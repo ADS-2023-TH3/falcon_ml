78b3e826eef1c35130b3673449737990ffad0008
arturofredes <arturofredesc@gmail.com>
2023-11-23 14:47:37 +0100

    writing functions

---
+++ src/writing_functions.py
@@ -0,0 +1,57 @@
+import datetime as dt
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+from gspread_dataframe import set_with_dataframe
+def connect_to_sheet(worksheet_name):
+    # Define the scope and credentials
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("movierecommender-405816-41309bde9020.json", scope)
+    file_id = '1aJPqNvbLqZg1N1iyx6IrGVYxgKypDhRwgfY3Xkw-I4o'
+    # Authenticate with Google Sheets
+    gc = gspread.authorize(credentials)
+
+    try:
+        # Open the Google Sheets file by its file ID
+        sh = gc.open_by_key(file_id)
+
+        # Open the specific worksheet by its name
+        worksheet = sh.worksheet(worksheet_name)
+
+        # Now, 'worksheet' represents the specific worksheet you want to work with
+        return worksheet
+    except gspread.exceptions.WorksheetNotFound:
+        print(f"Worksheet '{worksheet_name}' not found.")
+        return None
+
+def Sheet_to_df(worksheet):
+    if worksheet:
+        # Read the first row of data as column names
+        column_names = worksheet.row_values(1)
+
+        # Read the remaining data from the worksheet, excluding the first row
+        data = worksheet.get_all_values()[1:]
+
+        # Create a DataFrame with the data and set column names
+        df = pd.DataFrame(data, columns=column_names)
+
+        return df
+    else:
+        return None
+    
+
+    
+def add_log(user, input, output):
+    worksheet=connect_to_sheet('logs_sheet')
+    data = {
+    "user": [user],
+    "date": [dt.now().strftime("%Y-%m-%d")],
+    "input": [input],
+    "output": [output]
+    }
+    df = pd.DataFrame(data)
+    set_with_dataframe(worksheet, df, col=1,row=len(worksheet.get_all_records()) + 2,include_column_header=False)
+
+
+
+add_log('Arturo','in','out')
