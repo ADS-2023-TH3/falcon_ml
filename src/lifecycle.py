e7488dfd9288d5f4b602c706c213a5e9ec79aca1
davidrosado4 <rosadodav4@gmail.com>
2023-11-25 12:41:03 +0100

    mlflow solve

--- src/lifecycle.py
+++ src/lifecycle.py
@@ -1,12 +1,9 @@
 from spotlight.cross_validation import user_based_train_test_split
-from spotlight.datasets.movielens import get_movielens_dataset
-from spotlight.sequence.implicit import ImplicitSequenceModel
-from spotlight.interactions import Interactions
-from spotlight.sequence.representations import CNNNet
+from spotlight.datasets.synthetic import generate_sequential
 from spotlight.evaluation import sequence_mrr_score
+from spotlight.sequence.implicit import ImplicitSequenceModel
+from spotlight.datasets.movielens import get_movielens_dataset
 import numpy as np
-import torch  # to save the model. Backbone done in torch.
-import pandas as pd
 import mlflow
 
 
@@ -49,62 +46,40 @@ def load_data_to_sequences(variant='20M',
                                       step_size=step_size)
         return dataset
 
+# Mlflow experiments
+# Set experiment name
 
-def train_ImplicitSec_model(train, model_type='cnn', save_model=True, n_iter=3, loss_type='bpr'):
-    """Function that trains and saves the recommender model ImplicitSequenceModel()
+experiment_name = "Mlflow Experiment"
+mlflow.set_experiment(experiment_name)
 
-    Args:
-        train (sequence): sequence data ready to train using the ImplicitSequenceModel()
-        model_type (str, optional): Sequence representation to use by the ImplicitSequenceModel.
-                                    Default to 'cnn'.
-                                    Possible inputs: 'cnn', 'pooling' or 'lstm'
-        save_model (boolean): Default to 'True', determins whether or not to save the model.
+# Load data
+train, test = load_data_to_sequences(df_split=True)
 
-    Returns:
-        model (spotlight.sequence.implicit.ImplicitSequenceModel): trained model
-    """
-    model = ImplicitSequenceModel(n_iter=n_iter,
-                                  representation=model_type,
-                                  loss=loss_type)
-    model.fit(train)
+# Set parameters
+n_iter = 3
+representation = 'cnn'
+loss = 'bpr'
 
-    return model, model_type, n_iter, loss_type
+# Create model
+model = ImplicitSequenceModel(n_iter=n_iter,
+                              representation=representation,
+                              loss=loss)
+print("Fitting the model...")
+# Train the model
+model.fit(train)
+print("Computing MRR...")
+# Evaluate the model
+mrr_list = sequence_mrr_score(model, test)
+mrr_mean = np.mean(mrr_list)
 
-
-def evaluate_model(test, model):
-    """evaluates the trained model by computing the mrr metrics
-
-    Args:
-        test (sequence): _description_
-        model (spotlight.sequence.implicit.ImplicitSequenceModel): _description_
-
-    Returns:
-        mrr (np.array): mrr metrics
-    """
-    mrr = sequence_mrr_score(model, test)
-    print('MRR: ', mrr)
-
-    return mrr
-
-
-# Mlflow run
+# Log parameters
 with mlflow.start_run():
-    # Load data
-    train = load_data_to_sequences()
-
-    # Train model
-    model, model_type, n_iter, loss_type = train_ImplicitSec_model(train, model_type='cnn', save_model=True, n_iter=3, loss_type='bpr')
-
-    # Evaluate model
-    mrr = evaluate_model(train, model)
-
-    # Log parameters
-    mlflow.log_param("model_type", model_type)
     mlflow.log_param("n_iter", n_iter)
-    mlflow.log_param("loss_type", loss_type)
+    mlflow.log_param("representation", representation)
+    mlflow.log_param("loss", loss)
 
-    # Log metrics
-    mlflow.log_metric("mrr", mrr)
+    # Log the model
+    mlflow.sklearn.log_model(model, "model")
 
-    # Log artifacts (output files)
-    #mlflow.pytorch.log_model(model, "models")
+    # Log the mean of MRR scores
+    mlflow.log_metric("mrr_mean", mrr_mean)
